65c7e891df59bc3f57ed6eabe424fd20cd9c7c14
umarmhmd13-coder <umarmhmd13@gmail.com>
2025-12-24 19:36:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+﻿const API = "http://127.0.0.1:18080";
+
+function upload() {
+    const title = document.getElementById("title").value;
+    const teacher = document.getElementById("teacher").value;
+    const file = document.getElementById("file").files[0];
+
+    if (!file) {
+        alert("اختار فيديو");
+        return;
+    }
+
+    const form = new FormData();
+    form.append("title", title);
+    form.append("teacher", teacher);
+    form.append("file", file);
+
+    fetch(API + "/upload", {
+        method: "POST",
+        body: form
+    })
+        .then(res => res.text())
+        .then(() => {
+            alert("Upload Success ✅");
+            loadVideos();
+        })
+        .catch(() => alert("Backend مش شغال ❌"));
+}
